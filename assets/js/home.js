4564728b927061cef8b0b1ee03a2c3316b79b895
Julie Salha <julie.salha@gmail.com>
2026-09-23 08:04:32 +0200

    CTA order

--- assets/js/home.js
+++ assets/js/home.js
@@ -11,15 +11,6 @@ document.addEventListener("DOMContentLoaded", function () {
                     <p>Audit RGAA, Conseil, Accompagnement, Formation</p>
                 </div> 
             </div>          
-            <div class="actions"
-                >     
-                    <a href="https://tally.so/r/D4ZjzZ"
-                    
-                        title="“Recevoir mon mini-audit gratuit” - lien externe"
-                        target="_blank"
-                        rel="noreferrer noopener"
-                    >Recevoir mon mini-audit gratuit</a> 
-            </div>
             <div class="needs">
                 <p>Vous avez besoin : </p>
                 <ul>
@@ -29,6 +20,15 @@ document.addEventListener("DOMContentLoaded", function () {
                     <li>de former vos équipes ?</li>
                     <li>de disposer d'une experte accessibilité à vos côtés ?</li>
                 </ul>
+            </div>
+            <div class="actions">     
+                <a href="https://tally.so/r/D4ZjzZ"
+                        title="“Recevoir mon mini-audit gratuit” - lien externe"
+                        target="_blank"
+                        rel="noreferrer noopener"
+                    >
+                    Recevoir mon mini-audit gratuit
+                </a> 
             </div>
         </div>
         <figure class="is-svg image-square lightbox-figure">
